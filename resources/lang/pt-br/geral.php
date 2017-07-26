40d07b025df11331254ecd2729b2adb26fe77fd7
Henrique Freitas <henriqusouza90@gmail.com>
2017-07-25 22:52:13 -0300

    -Removido pasta vazia;

---
+++ resources/lang/pt-br/geral.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'registro-inserido-sucesso' => 'Registro inserido com sucesso!',
+    'registro-alterado-sucesso' => 'Registro alterado com sucesso!',
+    'registro-removido-sucesso' => 'Registro removido com sucesso!',
+
+    'aviso-remover' => 'Você deseja realmente remover esse registro?',
+];
